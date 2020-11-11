14d82dbf8394adbc0a61fb27689e15caafd6ab28
nzuain <nicolaszuain3@hotmail.com>
2020-11-11 00:26:04 -0300

    Version 1.024

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,6 +4,7 @@ var config = require('../config');
 var whitelist = ['http://localhost:5000', 'https://localhost:5000', 'http://localhost:3000', 'http://192.168.176.1', 'https://buscamina.herokuapp.com:'+config.server.port ]
 var corsOptions = {
     origin: function (origin, callback) {
+        console.log(origin)
         console.log(config.server.port);
         console.log(config.server.url);
         if (!origin || whitelist.indexOf(origin) !== -1) {
